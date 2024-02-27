4197f228bbb79e688e29b8b1b5a0756003c2562e
Jinen Setpal <jinens8@gmail.com>
2024-02-27 16:53:18 -0500

    added train-valid-test splits

--- src/const.py
+++ src/const.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+import random
 import torch
 
+# paths
 BASE_DIR = Path(__file__).parent.parent
 MODEL_DIR = BASE_DIR / 'models'
 DATA_DIR = BASE_DIR / 'data'
 FEATURES_DIR = DATA_DIR / 'expression' / '634da99d771a5b05d92a59cf'
 
+# data
+SPLITS = ['train', 'valid', 'test']
+SPLIT_WEIGHTS = [0.7, 0.05, 0.25]
+N_FEATURES = 29066
+SEED = 10
+
 # training
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+LR_BOUNDS = [1E-4, 1E-1]
 MODEL_NAME = 'default'
 LEARNING_RATE = 1E-3
-LR_BOUNDS = [1E-4, 1E-1]
-N_FEATURES = 29066
 HIDDEN_SIZE = 768
 BATCH_SIZE = 1024
 EPOCHS = 50
@@ -22,3 +29,5 @@ DEPTH = 5
 # logging
 MLFLOW_TRACKING_URI = 'https://dagshub.com/jinensetpal/interpretable-embeddings.mlflow'
 LOG_REMOTE = True
+
+random.seed(SEED)
--- src/data.py
+++ src/data.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 import scanpy as sc
+import random
 import torch
 
 from src import const
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(self, split='all'):
+        random.seed(const.SEED)
         self.data = sc.read_mtx(const.FEATURES_DIR / 'RNA_5prime_data.mtx').to_df().T
+
+        if split in const.SPLITS:
+            self.data['split'] = random.choices(const.SPLITS, weights=const.SPLIT_WEIGHTS, k=len(self.data))
+            self.data = self.data[self.data['split'] == split]
+            self.data.pop('split')
 
     def __len__(self):
         return len(self.data)
