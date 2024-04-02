0b1ccd5186dfcd69c3184935fb943d0eeb91e2f7
Harnish Modi <30519780+harn1shmodi@users.noreply.github.com>
2024-04-02 15:25:48 -0400

    Update pca.py

--- src/evaluation/encoders/pca.py
+++ src/evaluation/encoders/pca.py
@@ -5,14 +5,18 @@ import pickle
 from src.model.arch import Model
 from .base import BaseEncoder
 from src import const
+from sklearn.decomposition import PCA, IncrementalPCA
+import os
 
+PATH = os.path.join(os.path.dirname(__file__), '../../../pca.pickle')
 
 class PCA(BaseEncoder):
     def __init__(self, model_name=const.MODEL_NAME):
         #model = unpickle in new pca.py
-        with open('pca.pickle', 'rb') as f:
+        with open(PATH, 'rb') as f:
             pca = pickle.load(f)
         self.model = pca
 
     def encode(self, batch):
         #batch wise transform sing pca
+        return self.model.transform(batch)
