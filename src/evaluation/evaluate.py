7a040502a12c6ab1ec89a18faf4a438180f39a7d
dehruv <dnaraya@purdue.edu>
2024-04-02 18:37:06 -0400

    Added Diffmap

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -7,18 +7,22 @@ import torch
 from src.evaluation.encoders.autoencoder import AutoEncoder
 from src.data import Dataset
 from src import const
+from src.evaluation.encoders.diffmap import DiffMap
 
 
 def fit(model, encoder, optimizer, scheduler, loss, dataloader):
-    if const.LOG_REMOTE: mlflow.set_tracking_uri(const.MLFLOW_TRACKING_URI)
+    if const.LOG_REMOTE:
+        mlflow.set_tracking_uri(const.MLFLOW_TRACKING_URI)
     with mlflow.start_run():
         # log hyperparameters
-        mlflow.log_params({k: v for k, v in const.__dict__.items() if k == k.upper() and all(s not in k for s in ['DIR', 'PATH'])})
+        mlflow.log_params({k: v for k, v in const.__dict__.items(
+        ) if k == k.upper() and all(s not in k for s in ['DIR', 'PATH'])})
         mlflow.log_param('optimizer_fn', 'Adam')
 
         interval = max(1, (const.EPOCHS // 10))
         for epoch in range(const.EPOCHS):
-            if not (epoch+1) % interval: print('-' * 10)
+            if not (epoch+1) % interval:
+                print('-' * 10)
             epoch_loss = torch.empty(1)
 
             for X, y in dataloader:
@@ -31,12 +35,14 @@ def fit(model, encoder, optimizer, scheduler, loss, dataloader):
                 batch_loss.backward()
                 optimizer.step()
 
-                epoch_loss = torch.vstack([epoch_loss.to(const.DEVICE), batch_loss])
+                epoch_loss = torch.vstack(
+                    [epoch_loss.to(const.DEVICE), batch_loss])
             metrics = {'bce_loss': epoch_loss[1:].mean().item()}
             mlflow.log_metrics(metrics, step=epoch)
             if not (epoch+1) % interval:
                 print(f'epoch\t\t\t: {epoch+1}')
-                for key in metrics: print(f'{key}\t\t: {metrics[key]}')
+                for key in metrics:
+                    print(f'{key}\t\t: {metrics[key]}')
         print('-' * 10)
 
 
@@ -47,7 +53,8 @@ def evaluate(classifier, encoder):
     n_corr = 0
     for X, y in dataloader:
         with torch.no_grad():
-            n_corr += ((classifier(encoder.encode(X.to(const.DEVICE))) > 0.5).to(torch.int) == y.to(const.DEVICE)).sum()
+            n_corr += ((classifier(encoder.encode(X.to(const.DEVICE)))
+                       > 0.5).to(torch.int) == y.to(const.DEVICE)).sum()
 
     print(f'Accuracy: {n_corr/len(dataloader.dataset)*100}%')
 
@@ -56,7 +63,7 @@ if __name__ == '__main__':
     dataloader = torch.utils.data.DataLoader(Dataset('train'),
                                              batch_size=const.BATCH_SIZE,
                                              shuffle=True)
-    encoder = AutoEncoder()  # change this!
+    encoder = DiffMap()  # change this!
     classifier = nn.Sequential(nn.Linear(768, 1),
                                nn.Sigmoid()).to(const.DEVICE)
     optimizer = torch.optim.Adam(classifier.parameters(),
@@ -68,5 +75,6 @@ if __name__ == '__main__':
     fit(classifier, encoder, optimizer, scheduler, nn.BCELoss(), dataloader)
 
     classifier.eval()
-    torch.save(classifier.state_dict(), const.MODEL_DIR / f'{const.MODEL_NAME}_cls_head.pt')
+    torch.save(classifier.state_dict(), const.MODEL_DIR /
+               f'{const.MODEL_NAME}_cls_head.pt')
     evaluate(classifier, encoder)
