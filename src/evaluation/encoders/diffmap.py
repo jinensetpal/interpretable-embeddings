415c9feb952133ea55690d5c7008bcde97ab268a
dehruv <dnaraya@purdue.edu>
2024-04-02 20:28:24 -0400

    fixed sbatch

--- src/evaluation/encoders/diffmap.py
+++ src/evaluation/encoders/diffmap.py
@@ -5,11 +5,12 @@ from .base import BaseEncoder
 from src import const
 import scanpy as sc
 
+
 class DiffMap(BaseEncoder):
     def __init__(self):
         super().__init__()
 
     def encode(self, batch):
         sc.pp.neighbors(batch, 50)
-        res = sc.tl.diffmap(batch, 50, copy = True)
-        return res.obsm["X_diffmap"][:,1]
+        res = sc.tl.diffmap(batch, 50, copy=True)
+        return torch.tensor(res.obsm["X_diffmap"][:, 1]).to(const.DEVICE)
