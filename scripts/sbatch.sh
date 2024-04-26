3200f0e0b812e65eb38c3080c7dc1d050f3704b6
Dhruv Narayanan <dnaraya@scholar-fe05.rcac.purdue.edu>
2024-04-26 19:45:18 -0400

    debugging

--- scripts/sbatch.sh
+++ scripts/sbatch.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --account=gpu
 #SBATCH --gres=gpu:1
-#SBATCH -N 2
-#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH -n 1
 #SBATCH -c 6
 #SBATCH --constraint=V100_32GB
-#SBATCH --time=3:30:00
+#SBATCH --time=1:15:00
 
 module load cuda cudnn anaconda
 source activate dhruv
